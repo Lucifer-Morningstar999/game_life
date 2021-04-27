a3b4599e1f3492cf887ad01e99c8b07a1470fc05
Denis-2006 <74256767+Denis-2006@users.noreply.github.com>
2021-04-27 19:42:37 +0400

    Add files via upload

--- игра жизнь.py
+++ игра жизнь.py
@@ -3,6 +3,7 @@ import random
 import time
 
 def print_mass(a):
+    os.system("cls")
     for i in a:
         print(i)
 
@@ -28,20 +29,26 @@ def my_count(a, i, j):
 
 os.system("cls")
 
-n = 5
+n = 30
 a = []
 #while True:  
 for i in range(n):
     a.append([random.randrange(2) for i in range(n)]) #генерировали в каждом элементе массива другой массив длинной в m
-print_mass(a)
+#print_mass(a)
 #[random.randrange(0,3,2) for j in range(m)]
-for i in range(1,len(a)-1):
-    for j in range(1,len(a[i])-1):
-        if a[i][j] == 0 and my_count(a,i,j) res == 3:
-            a[i][j] = 1
-        if a[i][j] == 0 and my_count(a,i,j) res == 2 or res == 3:
-            a[i][j] = 1
-        else:
-            a[i][j] = 0
+while True:
+    temp = []
+    temp = a.copy()
+    for i in range(1,len(a)-1):
+        for j in range(1,len(a[i])-1):
+            if (a[i][j] == 1 and my_count(a,i,j) == 2) or my_count(a,i,j) == 3:
+                temp[i][j] = 1
+            else:
+                temp[i][j] = 0
+    a = temp.copy()
+    print_mass(a)
+    time.sleep(2)
+#print (my_count(a,i,j))
     #time.sleep(0.9)
-    print("==============================")
+#print("==============================")
+
